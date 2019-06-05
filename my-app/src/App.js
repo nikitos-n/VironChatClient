71749b180691f7dde5b498dac8e1cd375210c076
Nikita Nikolskiy <Nikita27142914@yandex.ru>
2019-06-05 15:43:16 +0300

    client files

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; 
+
+function App() {
+  return (
+    <div>
+      <div className="container-fluid">
+        <input className="btn btn-outline-danger" type="submit" value="Войти" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
