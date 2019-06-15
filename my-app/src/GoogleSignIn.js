79cfb982bc5cbd3be37d1c93efdd6da462ea30f5
Nikita Nikolskiy <Nikita27142914@yandex.ru>
2019-06-15 13:42:15 +0300

    Сделал авторизацию Google Sign In

--- my-app/src/GoogleSignIn.js
+++ my-app/src/GoogleSignIn.js
@@ -3,27 +3,39 @@ import GoogleLogin from 'react-google-login';
 import axios from 'axios';
 
 class GoogleSignIn extends React.Component {
+
+  constructor(){
+    super();
+    this.setState({
+      visible: true
+    })
+  }
     
   responseGoogle = (response) => {
-      console.log(response);
-      axios.post("http://localhost:3001/", response.Zi)//Получили токен и отправлем axios 
-        .then((response) => {//Ответ от сервера
-          console.log(response.data);
+      const tokenID = response.tokenId;
+      // console.log(tokenID);
+      axios.post("http://localhost:3001/", {tokenID})//Получили токен и отправлем axios 
+        .then((response) => {//Ответ от сервера(распакованный)
+          const name = response.data.given_name;
+          const surname = response.data.family_name;
+          console.log({name, surname});
+          this.renderRooms();
         });
     }
 
+    renderRooms = () =>{
+      this.visible=false;
+    }
 
     render() {
      return ( 
         <div className = "container" >
-          <div className = "row justify-content-center" > 
-           
+          <div className = "row justify-content-center" >   
           <GoogleLogin
             clientId="546694221893-u0otjmu2mtv0ughim8pmac70mnkgkdgl.apps.googleusercontent.com"
             buttonText="Login"
             onSuccess={this.responseGoogle}
-            cookiePolicy={'single_host_origin'}
-          />
+            cookiePolicy={'single_host_origin'} />
           </div> 
         </div>
         )
